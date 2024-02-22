f0d63d97d390cca3aed8f2f6e8cf1309c1908da6
chircea <david.chircea@gmail.com>
2024-02-22 12:37:48 +0100

    Pts 4

--- app/src/main/java/com/example/kotlinxml/MainActivity.kt
+++ app/src/main/java/com/example/kotlinxml/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.kotlinxml
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonChangeColor = findViewById<Button>(R.id.buttonChangeColor)
+        val buttonSwitchActivity = findViewById<Button>(R.id.buttonSwitchActivity)
+        val bodyLayout = findViewById<FrameLayout>(R.id.bodyLayout)
+
+        // Acción para cambiar el color de fondo del cuerpo
+        buttonChangeColor.setOnClickListener {
+            val newColor = Color.rgb((0..255).random(), (0..255).random(), (0..255).random())
+            bodyLayout.setBackgroundColor(newColor)
+        }
+
+        // Acción para cambiar a Activity 2
+        buttonSwitchActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/kotlinxml/MainActivity2.kt
+++ app/src/main/java/com/example/kotlinxml/MainActivity2.kt
@@ -1,11 +1,23 @@
 package com.example.kotlinxml
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val buttonSwitchActivity = findViewById<Button>(R.id.buttonSwitchActivity)
+
+        // Acción para cambiar a Activity 1
+        buttonSwitchActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
